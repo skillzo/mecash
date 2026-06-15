bc582cd3384e4e17f3919fd68bcefa5bbd5f6c4f
skillzo <skillzfire@yahoo.com>
2026-06-15 07:15:44 +0100

    initial commit

---
+++ src/main/java/com/mecash/mecash_api/MecashApiApplication.java
@@ -0,0 +1,13 @@
+package com.mecash.mecash_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecashApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mecash/mecash_api/MecashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecash.mecash_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
